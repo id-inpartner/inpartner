9e98854f2b0d476e30f1c19d28283419871cae36
Inpartner <inpartner@users.noreply.github.com>
2024-08-10 23:56:50 +0000

    Automated Build

--- .next/server/pages/sector/[slug].js
+++ .next/server/pages/sector/[slug].js
@@ -5,7 +5,7 @@ exports.id = 999;
 exports.ids = [999];
 exports.modules = {
 
-/***/ 703:
+/***/ 2153:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 
@@ -18,8 +18,6 @@ __webpack_require__.d(__webpack_exports__, {
 var jsx_runtime_ = __webpack_require__(5193);
 // EXTERNAL MODULE: ./src/components/Banner/index.tsx
 var Banner = __webpack_require__(9988);
-// EXTERNAL MODULE: external "react"
-var external_react_ = __webpack_require__(6689);
 // EXTERNAL MODULE: external "react-bootstrap/Container"
 var Container_ = __webpack_require__(4678);
 var Container_default = /*#__PURE__*/__webpack_require__.n(Container_);
@@ -44,6 +42,136 @@ const Description = /*#__PURE__*/ styled_default()("div", {
     target: "e9mois82"
 })("padding-top:1rem;& p{margin-bottom:1rem;}& li{margin-bottom:1rem;}");
 
+// EXTERNAL MODULE: ./src/components/Card/index.ts
+var Card = __webpack_require__(9740);
+// EXTERNAL MODULE: ./node_modules/next/image.js
+var next_image = __webpack_require__(5675);
+var image_default = /*#__PURE__*/__webpack_require__.n(next_image);
+;// CONCATENATED MODULE: ./src/containers/Sector/TrainingEducation/Infographic/Item.tsx
+
+
+
+
+const IconCircle = /*#__PURE__*/ styled_default()("div", {
+    target: "e192a7og0"
+})("padding:16px;border-radius:50%;border-width:8px;border-style:solid;border-color:", ({ titleBackgroundColor  })=>titleBackgroundColor, ";align-self:center;background-color:white;");
+const Item_Title = /*#__PURE__*/ styled_default()("div", {
+    target: "e192a7og1"
+})("font-weight:bold;font-size:24px;color:white;background-color:", ({ titleBackgroundColor  })=>titleBackgroundColor, ";display:flex;align-items:center;justify-content:center;min-height:88px;border-radius:8px;text-align:center;");
+const Triangle = /*#__PURE__*/ styled_default()("div", {
+    target: "e192a7og2"
+})("width:0;height:0;border-left:16px solid transparent;border-right:16px solid transparent;border-top:24px solid\n    ", ({ titleBackgroundColor  })=>titleBackgroundColor, ";align-self:center;");
+const C = /*#__PURE__*/ styled_default()(Card/* default */.ZP, {
+    target: "e192a7og3"
+})("background-color:white;display:flex;flex-direction:column;justify-content:stretch;flex:1;");
+const ItemContainer = /*#__PURE__*/ styled_default()("div", {
+    target: "e192a7og4"
+})("flex:1;display:flex;flex-direction:column;margin-left:16px;margin-right:16px;margin-top:24px;min-width:250px;z-index:1;");
+const Vertical = /*#__PURE__*/ styled_default()("div", {
+    target: "e192a7og5"
+})("background-color:black;width:2px;min-height:32px;align-self:center;");
+const Item_Description = /*#__PURE__*/ styled_default()("div", {
+    target: "e192a7og6"
+})("font-size:20px;text-align:center;margin:16px;");
+const Item = ({ icon , title , description , titleBackgroundColor  })=>{
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(ItemContainer, {
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx(IconCircle, {
+                titleBackgroundColor: titleBackgroundColor,
+                children: /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                    src: icon,
+                    width: 74,
+                    height: 74,
+                    quality: 100,
+                    alt: title
+                })
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(Vertical, {}),
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)(C, {
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx(Item_Title, {
+                        titleBackgroundColor: titleBackgroundColor,
+                        children: title
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx(Triangle, {
+                        titleBackgroundColor: titleBackgroundColor
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx(Item_Description, {
+                        children: description
+                    })
+                ]
+            })
+        ]
+    });
+};
+/* harmony default export */ const Infographic_Item = (Item);
+
+;// CONCATENATED MODULE: ./src/containers/Sector/TrainingEducation/Infographic/trainer.png
+/* harmony default export */ const trainer = ({"src":"/_next/static/media/trainer.10e228d3.png","height":74,"width":74,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAMAAADz0U65AAAAGFBMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABWNxwqAAAACHRSTlMCXhtRcjcjgrdH3XoAAAAJcEhZcwAACxMAAAsTAQCanBgAAAAwSURBVHicHYqxAQAgDINIk9b/P1bLAgMANktP90ZJtXbiXxprBB1JSlM5cFL/gfdcEF8AgFsHPfkAAAAASUVORK5CYII=","blurWidth":8,"blurHeight":8});
+;// CONCATENATED MODULE: ./src/containers/Sector/TrainingEducation/Infographic/time.png
+/* harmony default export */ const time = ({"src":"/_next/static/media/time.7d449948.png","height":70,"width":70,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAMAAADz0U65AAAAJFBMVEUAAABMaXEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB1rdTyAAAADHRSTlOYAKVPZBiCNihAcc75dcgLAAAACXBIWXMAAAsTAAALEwEAmpwYAAAANUlEQVR4nBXKORLAIAzAQMkHGPL//2ZotI1QiRfN4B5x0QOymeOjyg+ryOVsV2Lk+8UmgvYHGYUAuAZJbUwAAAAASUVORK5CYII=","blurWidth":8,"blurHeight":8});
+;// CONCATENATED MODULE: ./src/containers/Sector/TrainingEducation/Infographic/career.png
+/* harmony default export */ const career = ({"src":"/_next/static/media/career.04fd203f.png","height":72,"width":72,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAMAAADz0U65AAAAJFBMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACmWAJHAAAADHRSTlMVAWZxI0uifVgriTOa1uLeAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAN0lEQVR4nCXJuQHAMAwDsSP12t5/3xRBC6S0UyLd3e0kCqACo3PEEtczL+qvqMvO+s0NlCfX6AMgHQEC+GOjKQAAAABJRU5ErkJggg==","blurWidth":8,"blurHeight":8});
+;// CONCATENATED MODULE: ./src/containers/Sector/TrainingEducation/Infographic/book.png
+/* harmony default export */ const book = ({"src":"/_next/static/media/book.0d464641.png","height":82,"width":82,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAMAAADz0U65AAAAKlBMVEUAAAAAAAAAAAAAAABMaXEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD3h1zXAAAADnRSTlMkVl5qAHwdlBI+CQFkOSiB1uMAAAAJcEhZcwAACxMAAAsTAQCanBgAAAA4SURBVHicFclBDsAwCANBmyyQpO3/v1txG2nEI0kX4QznQixHOAafdN7BhprKdOSUu7sPgqqqzQ8iYgER6ybP5QAAAABJRU5ErkJggg==","blurWidth":8,"blurHeight":8});
+;// CONCATENATED MODULE: ./src/containers/Sector/TrainingEducation/Infographic/index.tsx
+
+
+
+
+
+
+
+
+const Container = /*#__PURE__*/ styled_default()("div", {
+    target: "e1l4pg2s0"
+})("display:flex;flex-wrap:wrap;justify-content:space-evenly;margin-left:-12px;margin-right:-12px;margin-bottom:28px;align-items:stretch;position:relative;");
+const Infographic_Title = /*#__PURE__*/ styled_default()("h2", {
+    target: "e1l4pg2s1"
+})("text-align:center;width:100%;font-weight:600;font-size:20px;margin-top:40px;margin-bottom:20px;@media (min-width:", GlobalStyle/* breakpoints.md */.AV.md, "){font-size:28px;margin-top:64px;margin-bottom:32px;}");
+const Dots = /*#__PURE__*/ styled_default()("div", {
+    target: "e1l4pg2s2"
+})("display:none;@media (min-width:", GlobalStyle/* breakpoints.lg */.AV.lg, "){position:absolute;display:block;border-top:2px dotted black;left:160px;right:160px;top:86px;z-index:0;}");
+const Infographic = ()=>{
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx(Infographic_Title, {
+                children: "Why Choose Inpartner?"
+            }),
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)(Container, {
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx(Dots, {}),
+                    /*#__PURE__*/ jsx_runtime_.jsx(Infographic_Item, {
+                        icon: trainer,
+                        titleBackgroundColor: "#1976D2",
+                        title: "Expert Instructor",
+                        description: "Learn from industry experts with extensive experience and knowledge."
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx(Infographic_Item, {
+                        icon: time,
+                        titleBackgroundColor: "#1562AF",
+                        title: "Flexible Learning",
+                        description: "Access our courses online or in-person, with flexible scheduling to fit your busy lifestyle."
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx(Infographic_Item, {
+                        icon: career,
+                        titleBackgroundColor: "#1976D2",
+                        title: "Career Support",
+                        description: "Benefit from our career support services, including resume building, interview preparation, and job placement assistance."
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx(Infographic_Item, {
+                        icon: book,
+                        titleBackgroundColor: "#0C3B69",
+                        title: "Cutting-Edge Curriculum",
+                        description: "Stay ahead with our up-to-date curriculum, incorporating the latest trends and technologies."
+                    })
+                ]
+            })
+        ]
+    });
+};
+/* harmony default export */ const TrainingEducation_Infographic = (Infographic);
+
 ;// CONCATENATED MODULE: ./src/containers/Sector/index.tsx
 
 
@@ -52,9 +180,6 @@ const Description = /*#__PURE__*/ styled_default()("div", {
 
 
 const Index = ({ data  })=>{
-    const descs = (0,external_react_.useMemo)(()=>data.description.split(/[\r\n]+/), [
-        data.description
-    ]);
     return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
         children: [
             /*#__PURE__*/ jsx_runtime_.jsx(Banner/* default */.ZP, {
@@ -71,6 +196,7 @@ const Index = ({ data  })=>{
                             __html: data.description
                         }
                     }),
+                    data.slug == "education-training" && /*#__PURE__*/ jsx_runtime_.jsx(TrainingEducation_Infographic, {}),
                     /*#__PURE__*/ (0,jsx_runtime_.jsxs)(Projects, {
                         children: [
                             data.projects.map((p, i)=>/*#__PURE__*/ jsx_runtime_.jsx(Project/* default */.ZP, {
@@ -107,7 +233,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _emotion_react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_emotion_react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(968);
 /* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _containers_Sector__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(703);
+/* harmony import */ var _containers_Sector__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(2153);
 /* harmony import */ var _components_Navbar__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(9104);
 /* harmony import */ var _components_Footer__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(9154);
 var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([_components_Navbar__WEBPACK_IMPORTED_MODULE_3__]);
